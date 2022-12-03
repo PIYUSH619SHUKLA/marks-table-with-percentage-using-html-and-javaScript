7428e3ef2286f7627468203a921e33f93e8fecc6
PIYUSH SHUKLA <117061339+PIYUSH619SHUKLA@users.noreply.github.com>
2022-12-03 21:51:43 +0530

    Add files via upload

---
+++ marks.js
@@ -0,0 +1,35 @@
+document.write("Marks percentage=")
+alert("This page contains percentage");
+var m=70;
+var e=90;
+var p=90;
+var b=90;
+var c=90;
+var py=90;
+var avg=m+e+p+b+c+py
+var d=(avg/600).toFixed(4)
+var per=(d*100)
+document.write(per)
+document.write("</br>")
+// if(per>35&per<65)
+// document.write("You have passed and being promoted to next class")
+// else if(per>65&per<80)
+// document.write("second division")
+// else if(per>80)
+// document.write("Distinction")
+// switch(per)
+// {
+//     case(50):document.write("pass");
+//     break;
+//     case(60):document.write("second division");
+//     break;
+//     case(90):document.write("distinction");
+//     break;
+//     default:document.write("Fail")
+
+// }
+let i=1, n=10;
+while(i<=n){
+    console.log(i);
+    i+=2;
+}
